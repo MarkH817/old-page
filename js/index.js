154b9343834336afad2a66d54cbe24feac9daf2e
Mark Hernandez <markedcode@gmail.com>
2017-09-28 13:26:15 -0500

    Add resources directory and react

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+import React from 'react'
+import {render} from 'react-dom'
+
 setTimeout(() => {
   let splash = document.getElementById('splash')
   splash.classList.remove('invisible')
 }, 250)
+
+render(<h1>Hey!</h1>, document.getElementById('root'))
